80c7d090e97941d2047661a729b07e7ea7282e7f
Dennis Eichhorn <spl1nes.com@googlemail.com>
2020-03-05 20:35:58 +0100

    fixes Orange-Management/phpOMS#224 and fixes Orange-Management/phpOMS#212

---
+++ Models/NullQAQuestion.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 7.4
+ *
+ * @package   Modules\QA\Models
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 1.0
+ * @version   1.0.0
+ * @link      https://orange-management.org
+ */
+declare(strict_types=1);
+
+namespace Modules\QA\Models;
+
+/**
+ * Null model
+ *
+ * @package Modules\QA\Models
+ * @license OMS License 1.0
+ * @link    https://orange-management.org
+ * @since   1.0.0
+ */
+final class NullQAQuestion extends QAQuestion
+{
+    /**
+     * Constructor
+     *
+     * @param int $id Model id
+     *
+     * @since 1.0.0
+     */
+    public function __construct(int $id = 0)
+    {
+        $this->id = $id;
+    }
+}
