94d40f0f4c67745b6a9bd4d8641c13bc84d434fa
Dennis Eichhorn <spl1nes.com@googlemail.com>
2021-03-05 21:08:46 +0100

    draft additional languages

---
+++ Theme/Backend/Lang/ja.lang.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 8.0
+ *
+ * @package   Modules\Localization
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 1.0
+ * @version   1.0.0
+ * @link      https://orange-management.org
+ */
+declare(strict_types=1);
+
+return ['QA' => [
+    'Badges' => 'バッジ',
+    'List'   => 'リスト',
+    'Name'   => '名前',
+    'QA'     => 'QA',
+]];
---
+++ Theme/Backend/Lang/zh.lang.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 8.0
+ *
+ * @package   Modules\Localization
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 1.0
+ * @version   1.0.0
+ * @link      https://orange-management.org
+ */
+declare(strict_types=1);
+
+return ['QA' => [
+    'Badges' => '徽章',
+    'List'   => '列表',
+    'Name'   => '姓名',
+    'QA'     => 'QA',
+]];
