3c53c7d956846982b628b778414e280dcaab81c6
Dennis Eichhorn <spl1nes.com@googlemail.com>
2021-10-21 22:16:07 +0200

    more tests and make classes final

---
+++ tests/Models/NullQAQuestion.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 8.0
+ *
+ * @package   tests
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 1.0
+ * @version   1.0.0
+ * @link      https://orange-management.org
+ */
+declare(strict_types=1);
+
+namespace Modules\QA\tests\Models;
+
+use Modules\QA\Models\NullQAQuestion;
+
+/**
+ * @internal
+ */
+final class Null extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @covers Modules\QA\Models\NullQAQuestion
+     * @group framework
+     */
+    public function testNull() : void
+    {
+        self::assertInstanceOf('\Modules\QA\Models\QAQuestion', new NullQAQuestion());
+    }
+
+    /**
+     * @covers Modules\QA\Models\NullQAQuestion
+     * @group framework
+     */
+    public function testId() : void
+    {
+        $null = new NullQAQuestion(2);
+        self::assertEquals(2, $null->getId());
+    }
+}
