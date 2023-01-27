77abadbf587ca4b15bce69a63f44d639b6b15173
Dennis Eichhorn <spl1nes.com@googlemail.com>
2023-01-27 22:12:09 +0100

    phpstan, phpcs, phpunit fixes

--- Models/QAAnswerMapper.php
+++ Models/QAAnswerMapper.php
@@ -48,7 +48,7 @@ final class QAAnswerMapper extends DataMapperFactory
     /**
      * Belongs to.
      *
-     * @var array<string, array{mapper:string, external:string, column?:string, by?:string}>
+     * @var array<string, array{mapper:class-string, external:string, column?:string, by?:string}>
      * @since 1.0.0
      */
     public const BELONGS_TO = [
@@ -66,7 +66,7 @@ final class QAAnswerMapper extends DataMapperFactory
     /**
      * Has many relation.
      *
-     * @var array<string, array{mapper:string, table:string, self?:?string, external?:?string, column?:string}>
+     * @var array<string, array{mapper:class-string, table:string, self?:?string, external?:?string, column?:string}>
      * @since 1.0.0
      */
     public const HAS_MANY = [
--- Models/QAQuestionMapper.php
+++ Models/QAQuestionMapper.php
@@ -50,7 +50,7 @@ final class QAQuestionMapper extends DataMapperFactory
     /**
      * Has many relation.
      *
-     * @var array<string, array{mapper:string, table:string, self?:?string, external?:?string, column?:string}>
+     * @var array<string, array{mapper:class-string, table:string, self?:?string, external?:?string, column?:string}>
      * @since 1.0.0
      */
     public const HAS_MANY = [
@@ -83,7 +83,7 @@ final class QAQuestionMapper extends DataMapperFactory
     /**
      * Belongs to.
      *
-     * @var array<string, array{mapper:string, external:string, column?:string, by?:string}>
+     * @var array<string, array{mapper:class-string, external:string, column?:string, by?:string}>
      * @since 1.0.0
      */
     public const BELONGS_TO = [
