3d96b3913d225e47a955393fbfe52ac501ef146c
Dennis Eichhorn <spl1nes.com@googlemail.com>
2023-04-17 19:44:57 +0200

    test fixes

--- tests/Controller/ApiControllerTest.php
+++ tests/Controller/ApiControllerTest.php
@@ -26,6 +26,7 @@ use phpOMS\DataStorage\Session\HttpSession;
 use phpOMS\Dispatcher\Dispatcher;
 use phpOMS\Event\EventManager;
 use phpOMS\Localization\ISO639x1Enum;
+use phpOMS\Localization\L11nManager;
 use phpOMS\Message\Http\HttpRequest;
 use phpOMS\Message\Http\HttpResponse;
 use phpOMS\Message\Http\RequestStatusCode;
@@ -69,6 +70,7 @@ final class ApiControllerTest extends \PHPUnit\Framework\TestCase
         $this->app->eventManager   = new EventManager($this->app->dispatcher);
         $this->app->eventManager->importFromFile(__DIR__ . '/../../../../Web/Api/Hooks.php');
         $this->app->sessionManager = new HttpSession(36000);
+        $this->app->l11nManager    = new L11nManager();
 
         $account = new Account();
         TestUtils::setMember($account, 'id', 1);
