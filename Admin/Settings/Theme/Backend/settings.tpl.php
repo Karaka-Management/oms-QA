b3de619d3e6aa2adb44783aa5610b4def524153c
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-05-28 01:13:23 +0000

    style fixes, bug fixes

--- Admin/Settings/Theme/Backend/settings.tpl.php
+++ Admin/Settings/Theme/Backend/settings.tpl.php
@@ -25,7 +25,7 @@ echo $this->getData('nav')->render(); ?>
 <div class="row">
     <div class="col-xs-12">
         <div class="portlet">
-            <div class="portlet-head"><?= $this->getHtml('Apps'); ?><i class="fa fa-download floatRight download btn"></i></div>
+            <div class="portlet-head"><?= $this->getHtml('Apps'); ?><i class="lni lni-download download btn end-xs"></i></div>
             <div class="slider">
             <table id="appList" class="default sticky">
                 <thead>
